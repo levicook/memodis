30e1f702fabdcd6daab96dd86c145039a42682bc
Levi Cook <levicook@gmail.com>
2010-02-14 18:26:52 -0600

    short hand for common coders;

--- examples/main.rb
+++ examples/main.rb
@@ -9,22 +9,24 @@ def fib(num)
   fib(num - 1) + fib(num - 2)
 end
 
-print 'Before memoize: '
-puts Hitimes::Interval.measure { fib(30) }
+puts 'Before memoize: '
+puts Hitimes::Interval.measure { print fib(30); print ': ' }
 
 
 extend Memodis
 
 # TODO add namespace!!
 cache = Memodis::DistCache.new({
-  :decode => lambda { |val| Integer(val) },
-  :master => '127.0.0.1:16379 127.0.0.1:16380'.split,
-  :slaves => '127.0.0.1:16389 127.0.0.1:16390 
-              127.0.0.1:16391 127.0.0.1:16392
-              127.0.0.1:16393 127.0.0.1:16394'.split
+  :decoder => :integer,
+  :master  => '127.0.0.1:16379 127.0.0.1:16380'.split,
+  :slaves  => '127.0.0.1:16389 127.0.0.1:16390 
+               127.0.0.1:16391 127.0.0.1:16392
+               127.0.0.1:16393 127.0.0.1:16394'.split
 })
 
-memoize :fib, cache
+memoize :fib, cache 
 
-print ' After memoize: '
-puts Hitimes::Interval.measure { fib(30) }
+puts "\nAfter memoize: "
+puts Hitimes::Interval.measure { print fib(30); print ': ' }
+puts Hitimes::Interval.measure { print fib(30); print ': ' }
+puts Hitimes::Interval.measure { print fib(30); print ': ' }
--- lib/memodis/dist_cache.rb
+++ lib/memodis/dist_cache.rb
@@ -1,45 +1,56 @@
 module Memodis
 
-  class DistCache 
+  class DistCache
+
+    CODERS = {}
+    CODERS.default   = lambda { |v| v }
+    CODERS[:float]   = lambda { |v| Float(v) }
+    CODERS[:integer] = lambda { |v| Integer(v) }
+    CODERS.freeze
 
     def initialize(options)
       @master = DistRedis.new({
         :db => options[:db],
-        :hosts => options[:master], 
+        :hosts => options[:master],
         :timeout => options[:timeout],
       })
       @slaves = options.fetch(:slaves, []).map do |h|
         host, port = h.split(':')
         Redis.new({
-          :db => options[:db], 
-          :host => host, 
-          :port => port, 
+          :db => options[:db],
+          :host => host,
+          :port => port,
           :timeout => options[:timeout],
         })
       end
-      @encoder = options.fetch(:encode, default_coder)
-      @decoder = options.fetch(:decode, default_coder)
+      @encoder = options.fetch(:encoder, CODERS[options[:encoder]])
+      @decoder = options.fetch(:decoder, CODERS[options[:decoder]])
     end
 
-    def [](key)
-      decode(@master.get(key))
-    end
-      
-    def []=(key, val)
+    def []= key, val
       @master.set(key, encode(val))
+    end
+
+    def [] key
+      if val = get(key)
+        decode(val)
+      else
+        nil # don't decode a miss
+      end
     end
 
     private
 
-    def default_coder
-      lambda { |v| v }
+    def get key
+      ## TODO read from slaves
+      @master.get(key)
     end
 
     def decode(val)
       @decoder.call(val)
     end
 
-    def decode(val)
+    def encode(val)
       @encoder.call(val)
     end
 
